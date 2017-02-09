900ec13d05c5cdcde15f38169a1b57cc2691c214
iliuta <adrian.papusoi@gmail.com>
2017-02-09 11:26:35 +0100

    OAuth2 more integrated with Spring boot

--- src/main/java/com/iliuta/strava/StravaClientApplication.java
+++ src/main/java/com/iliuta/strava/StravaClientApplication.java
@@ -3,30 +3,34 @@ package com.iliuta.strava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 @SpringBootApplication
 @Configuration
-@EnableAutoConfiguration(exclude = {
-        org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class})
-@ImportResource("classpath:application-context.xml")
+// exclude spring security, we only need oauth2 client
+@EnableAutoConfiguration(exclude = {org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class})
+@EnableOAuth2Client
 public class StravaClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StravaClientApplication.class, args);
     }
 
-
+    /**
+     * Create oauth rest template (spring boot magic with configuration from application.properties)
+     *
+     * @param oauth2ClientContext
+     * @param details
+     * @return
+     */
     @Bean
-    public FilterRegistrationBean oAuth2ClientContextFilter() {
-        FilterRegistrationBean registration = new FilterRegistrationBean();
-        registration.setFilter(new OAuth2ClientContextFilter());
-        registration.setName("oAuth2ClientContextFilter");
-        registration.setOrder(1);
-        return registration;
+    public OAuth2RestTemplate stravaRestTemplate(OAuth2ClientContext oauth2ClientContext, OAuth2ProtectedResourceDetails details) {
+        OAuth2RestTemplate result = new OAuth2RestTemplate(details, oauth2ClientContext);
+        return result;
     }
 }
